12d6f9665efc1fe76c0503cf6385b0232e9e3b05
Sid Raval <sid@thoughtbot.com>
2015-11-19 15:52:24 -0500

    Separate top level array functions

--- Source/Extensions/NSURLRequest.swift
+++ Source/Extensions/NSURLRequest.swift
@@ -5,12 +5,27 @@ public extension NSURLRequest {
     let json = HTTPBody.flatMap { try? NSJSONSerialization.JSONObjectWithData($0, options: NSJSONReadingOptions(rawValue: 0)) }
     return (json as? [String: AnyObject]) ?? [:]
   }
+
+  var jsonPayloadArray: [AnyObject] {
+    let json = HTTPBody.flatMap { try? NSJSONSerialization.JSONObjectWithData($0, options: NSJSONReadingOptions(rawValue: 0)) }
+    return (json as? [AnyObject]) ?? []
+  }
 }
 
 public extension NSMutableURLRequest {
   override var jsonPayload: NSDictionary {
     get {
       return super.jsonPayload
+    }
+
+    set {
+      HTTPBody = try? NSJSONSerialization.dataWithJSONObject(newValue, options: NSJSONWritingOptions(rawValue: 0))
+    }
+  }
+
+  override var jsonPayloadArray: [AnyObject] {
+    get {
+      return super.jsonPayloadArray
     }
 
     set {
--- Tests/Tests/NSURLRequestSpec.swift
+++ Tests/Tests/NSURLRequestSpec.swift
@@ -28,5 +28,31 @@ class NSURLRequestSpec: QuickSpec {
         }
       }
     }
+
+    describe("jsonPayloadArray") {
+      context("when given an encodable object") {
+        it("serializes and deserializes correctly") {
+          let urlRequest = NSMutableURLRequest(URL: NSURL(string: "http://www.example.com")!)
+          urlRequest.jsonPayloadArray = [["ids": [0, 1, 2]]]
+
+          let payloadFromRequest = urlRequest.jsonPayloadArray
+          let object = payloadFromRequest.first! as! [String: AnyObject]
+          let ids: [Int]? = object["ids"] as? [Int]
+
+          expect(ids).to(equal([0, 1, 2]))
+        }
+      }
+
+      context("when the HTTPBody cannot be decoded") {
+        it("returns an empty dictionary") {
+          let urlRequest = NSMutableURLRequest(URL: NSURL(string: "http://www.example.com")!)
+          urlRequest.HTTPBody = NSData()
+
+          let payloadFromRequest = urlRequest.jsonPayloadArray
+
+          expect(payloadFromRequest.count).to(equal(0))
+        }
+      }
+    }
   }
 }
